7143fbd58298ce426fac06c34127c3655d69b446
samim5274 <banglarcele1122@gmail.com>
2026-07-28 15:32:33 +0600

    Sitemap update

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -2,24 +2,38 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
 <url>
-    <loc><?= url('/') ?></loc>
-    <lastmod><?= now()->toAtomString() ?></lastmod>
+    <loc>{{ url('/') }}</loc>
+    <lastmod>{{ now()->toAtomString() }}</lastmod>
     <changefreq>daily</changefreq>
     <priority>1.0</priority>
 </url>
 
-<?php foreach ($categories as $category): ?>
-<url>
-    <loc><?= url('/category/' . $category->slug) ?></loc>
-    <lastmod><?= optional($category->updated_at)->toAtomString() ?></lastmod>
-</url>
-<?php endforeach; ?>
 
-<?php foreach ($products as $product): ?>
+{{-- Categories --}}
+@foreach($categories as $category)
+
 <url>
-    <loc><?= url('/product-details/' . $product->slug) ?></loc>
-    <lastmod><?= optional($product->updated_at)->toAtomString() ?></lastmod>
+    <loc>{{ url('/category/' . $category->slug . '/' . $category->id) }}</loc>
+    <lastmod>{{ optional($category->updated_at)->toAtomString() }}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
 </url>
-<?php endforeach; ?>
+
+@endforeach
+
+
+
+{{-- Products --}}
+@foreach($products as $product)
+
+<url>
+    <loc>{{ url('/product-details/' . $product->slug) }}</loc>
+    <lastmod>{{ optional($product->updated_at)->toAtomString() }}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.7</priority>
+</url>
+
+@endforeach
+
 
 </urlset>
