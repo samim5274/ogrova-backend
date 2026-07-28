50904794cb4b292c600326f68d593214953d1479
samim5274 <banglarcele1122@gmail.com>
2026-07-28 16:13:57 +0600

    Sitemap remove

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Auth\SocialAuthController;
-use App\Http\Controllers\SiteMap\SitemapController;
 
 Route::get('/', function () {
     return redirect('https://ogrova.mercuviax.com/');
@@ -43,6 +42,3 @@ Route::get('/auth/{provider}/callback', [SocialAuthController::class,'callback']
 // Route::get('auth/facebook/callback', [SocialAuthController::class, 'loginWithFacebook']);
 // Route::get('auth/google/callback', [SocialAuthController::class, 'loginWithGoogle']);
 // Route::get('auth/github/callback', [SocialAuthController::class, 'loginWithGithub']);
-
-// Site map
-Route::get('/sitemap.xml', [SitemapController::class, 'index']);
